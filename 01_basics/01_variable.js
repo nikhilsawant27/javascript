bc6e29b495a23a69f5d1d7939ea38b8129d37f79
nikhilsawant27 <nsawant2003@gmail.com>
2025-06-11 06:45:09 +0000

    container config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12
+let name = "nikhil"
+var accountEmail="nikhikgoogle.com"
+accountPassword="1234"
+
+/*
+Prefer to not to used var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,name,accountEmail,accountPassword]);
