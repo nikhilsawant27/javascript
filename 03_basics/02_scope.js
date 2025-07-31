ba6755ac706a265f03099d5bb80dd9417796c4f2
nikhilsawant27 <nsawant2003@gmail.com>
2025-07-31 20:44:32 +0000

    conatiner config

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,4 +8,32 @@ if(true){
 
 // console.log(s);
 // console.log(e);
-console.log(f);         // that why var is avoided
+// console.log(f);         // that why var is avoided
+
+///      nested scope
+
+// function one(){
+//     const userName = "hitesh"
+//     function two (){
+//         const website = "w3schoole.com"
+//     //     console.log(userName);
+    // }
+    // console.log(website);
+//     two()
+    
+// }
+// one()
+
+// +++++++++++++++++++++ interesting+++++++++++++++
+
+function addOwo(num){
+    return num+4
+}
+addOwo(3)                                 // two ways function is declare
+
+
+const addTwo = function(num){
+    return num+3
+}
+
+addTwo(6) 
