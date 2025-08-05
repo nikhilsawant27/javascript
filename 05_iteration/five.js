64d6b9bcd8059f0f3640125695cad375e4c6742a
nikhilsawant27 <nsawant2003@gmail.com>
2025-08-05 17:11:43 +0000

    container config

---
+++ 05_iteration/five.js
@@ -0,0 +1,43 @@
+// const coding=["js","cpp","java","python"]
+
+// coding.forEach( function (val){
+//     console.log(val );
+    
+// })
+
+// coding.forEach((iteam)=>{
+//     console.log(iteam);
+    
+// })
+// function printMe(item){
+//     console.log(item);
+                              // for this we have to give only reference regarding to it  
+}
+
+// coding.forEach(printMe)
+// coding.forEach((item,index,arr) => {
+//     console.log(item,index,arr);
+    
+// })
+
+// const myCoding = [
+//     {
+//         languageName:"javasccript",
+//         languageFile:"js",
+//         languageFileName:"java"
+//     },
+//     {
+
+//     },
+//     {
+
+//     },
+
+// ]
+
+// myCoding.forEach( (item) => {
+//  console.log(item.languageName);
+    
+// }
+   
+// )
---
+++ 05_iteration/four.js
@@ -0,0 +1,16 @@
+const myObject={
+    'game1':'cs',
+    'game2': 'batman'
+    
+}
+for (const key in myObject) {
+//    console.log(`${key}shortcut is ${myObject[key]}`);
+   
+    }
+
+    const programming =["js","python","java","html"]
+
+    for (const key in programming) {
+        // console.log(`programing ${key} shortform is${programming}`);
+        console.log(programming[key]); 
+    }
